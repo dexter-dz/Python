00bbdb4715d55cad61e80fc79e466fb37222f8ab
dexter-dz <dineshzaladekar@yahoo.in>
2020-09-03 06:04:23 +0530

    Tables
    If you want a program that calculates tables for you. Here you go

---
+++ table.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+num	=int(input("which table you want to see")) #Number which you want to see table of
+a = 21 # Table till the nth no -1
+n,n1 = 1,num
+
+if num	<=0: 			#error code if someone enters 0
+	print("enter a positive no")
+elif num	==1: 		#you know what will happen if you multiply 1
+	print("you are too weak")
+else:  					#The actual program
+	print ("Table of " +str(num) +" is")
+	while n < a:
+		
+		n1 = num * n
+		n += 1	
+		print (n1)
